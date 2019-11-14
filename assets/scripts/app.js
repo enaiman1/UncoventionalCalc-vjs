c5f2b9070499564bf93d5fd711f70736471bed5e
Ericcentrik <enaiman1@gmail.com>
2019-11-14 07:55:02 -0500

    created a writeLog function to refector my my code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,46 +1,60 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 // Gets input from input field
-function getUserNumberInput(){
+function getUserNumberInput() {
     return parseInt(userInput.value)
 }
 // Generate and writes calculation log
-function createAndwriteOutput(operator, resultBeforeCalc, calcNumber){
+function createAndwriteOutput(operator, resultBeforeCalc, calcNumber) {
     const calDesc = `${resultBeforeCalc} ${operator} ${calcNumber}`;
     outputResult(currentResult, calDesc)//from vendor.js file
+}
+
+function writeToLog(operationIdentifer, prevResult, operationNumber, newResult){
+    const logEntry = {
+        operation: operationIdentifer,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries)
 }
 
 function add() {
     const enteredNumber = getUserNumberInput();
     const intialResult = currentResult;
-    currentResult = currentResult + enteredNumber;
-    createAndwriteOutput('+', intialResult, enteredNumber );
+    currentResult += enteredNumber;
+    createAndwriteOutput('+', intialResult, enteredNumber);
+    writeToLog('ADD', intialResult, enteredNumber, currentResult)
     
 }
 
-function subtract(){
+function subtract() {
+
     const enteredNumber = getUserNumberInput();
     const intialResult = currentResult;
-    currentResult = currentResult - enteredNumber;
-    createAndwriteOutput('-', intialResult, enteredNumber );
-    
+    currentResult -= enteredNumber;
+    createAndwriteOutput('+', intialResult, enteredNumber);
+    writeToLog('SUBTRACT', intialResult, enteredNumber, currentResult)
 }
 
-function multiply(){
+function multiply() {
     const enteredNumber = getUserNumberInput();
     const intialResult = currentResult;
-    currentResult = currentResult * enteredNumber;
-    createAndwriteOutput('*', intialResult, enteredNumber );
-    
+    currentResult *= enteredNumber;
+    createAndwriteOutput('*', intialResult, enteredNumber);
+    writeToLog('MULTYPLY', intialResult, enteredNumber, currentResult)
 }
 
 function divide() {
     const enteredNumber = getUserNumberInput();
     const intialResult = currentResult;
-    currentResult = currentResult / enteredNumber;
-    createAndwriteOutput('/', intialResult, enteredNumber );
-    
+    currentResult /= enteredNumber;
+    createAndwriteOutput('/', intialResult, enteredNumber);
+    writeToLog('DIVIDE', intialResult, enteredNumber, currentResult)
 }
 
 addBtn.addEventListener('click', add);
